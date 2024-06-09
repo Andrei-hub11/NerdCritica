6ff35f543dc0335f44ee67752c6088c90a5408a8
Andrei-hub11 <andreirodrigues794@gmail.com>
2024-06-08 22:36:35 -0300

    teste de unidades em helpers

---
+++ src/NerdCritica.TestProject/Helpers/CastMemberHelperTests.cs
@@ -0,0 +1,72 @@
+﻿using NerdCritica.Domain.Common;
+using NerdCritica.Domain.DTOs.Movie;
+using NerdCritica.Domain.Utils;
+
+namespace NerdCritica.TestProject.Helpers;
+
+public class CastMemberHelperTests
+{
+    [Fact]
+    public void GetCast_NullCastList_ThrowsArgumentException()
+    {
+        List<CastMemberRequestDTO> cast = new List<CastMemberRequestDTO>();
+        var castImagePaths = new Dictionary<string, CastImages>();
+
+        Assert.Throws<ArgumentException>(() => CastMemberHelper.GetCast(cast, castImagePaths));
+    }
+
+    [Fact]
+    public void GetCast_EmptyCastList_ThrowsArgumentException()
+    {
+        List<CastMemberRequestDTO> cast = new List<CastMemberRequestDTO>();
+        var castImagePaths = new Dictionary<string, CastImages>();
+
+        Assert.Throws<ArgumentException>(() => CastMemberHelper.GetCast(cast, castImagePaths));
+    }
+
+    [Fact]
+    public void GetCast_NullCastImagePaths_ThrowsArgumentException()
+    {
+        var cast = new List<CastMemberRequestDTO>
+    {
+        new CastMemberRequestDTO
+        (
+            MemberName: "Actor Name",
+            CharacterName: "Character Name",
+            MemberImage: "image",
+            RoleType: 1
+        )
+    };
+        Dictionary<string, CastImages> castImagePaths = new Dictionary<string, CastImages>();
+
+        Assert.Throws<ArgumentException>(() => CastMemberHelper.GetCast(cast, castImagePaths));
+    }
+
+    [Fact]
+    public void GetCast_ValidInput_GeneratesCorrectCastMembers()
+    {
+        var cast = new List<CastMemberRequestDTO>
+    {
+        new CastMemberRequestDTO
+        (
+            MemberName: "Actor Name",
+            CharacterName: "Character Name",
+            MemberImage: "image",
+            RoleType: 1
+        )
+    };
+
+        var castImage = CastImages.Create("users/images/" + Guid.NewGuid(), new byte[10]);
+
+        var castImagePaths = new Dictionary<string, CastImages>
+    {
+        { "Actor Name", castImage }
+    };
+
+        var result = CastMemberHelper.GetCast(cast, castImagePaths);
+
+        Assert.NotNull(result);
+        Assert.Single(result);
+        Assert.Equal("Actor Name", result[0].MemberName);
+    }
+}
---
+++ src/NerdCritica.TestProject/Helpers/CreateUserErrorHelperTests.cs
@@ -0,0 +1,49 @@
+﻿using NerdCritica.Domain.Utils.Exceptions;
+using NerdCritica.Domain.Utils;
+
+namespace NerdCritica.TestProject.Helpers;
+
+public class CreateUserErrorHelperTests
+{
+    [Fact]
+    public void GetExceptionFromResult_NoErrors_ReturnsGenericException()
+    {
+        var result = Result.Ok(true);
+
+        Assert.Throws<ArgumentException>(() => CreateUserErrorHelper.GetExceptionFromResult(result));
+    }
+
+    [Fact]
+    public void GetExceptionFromResult_DuplicateUserName_ReturnsCreateUserExceptionWithCorrectMessage()
+    {
+        var result = Result.Fail("DuplicateUserName");
+
+        var exception = CreateUserErrorHelper.GetExceptionFromResult(result);
+
+        Assert.IsType<CreateUserException>(exception);
+        Assert.Equal("O nome de usuário já está em uso. Escolha outro nome de usuário.", exception.Message);
+    }
+
+    [Fact]
+    public void GetExceptionFromResult_DuplicateEmail_ReturnsCreateUserExceptionWithCorrectMessage()
+    {
+        var result = Result.Fail("DuplicateEmail");
+
+
+        var exception = CreateUserErrorHelper.GetExceptionFromResult(result);
+
+        Assert.IsType<CreateUserException>(exception);
+        Assert.Equal("O e-mail já está em uso. Utilize outro endereço de e-mail.", exception.Message);
+    }
+
+    [Fact]
+    public void GetExceptionFromResult_UnknownError_ReturnsGenericCreateUserException()
+    {
+        var result = Result.Fail("UnknownError");
+
+        var exception = CreateUserErrorHelper.GetExceptionFromResult(result);
+
+        Assert.IsType<CreateUserException>(exception);
+        Assert.Equal("Algo deu errado ao criar o usuário.", exception.Message);
+    }
+}
