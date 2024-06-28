7aed60bae7f1a28fd12fe5553d0b5c948db87742
Andrei-hub11 <andreirodrigues794@gmail.com>
2024-06-28 14:55:11 -0300

    refatorando o ErrorHandlerMiddleware

--- src/NerdCritica.Api/Middleware/ErrorHandlerMiddleware.cs
+++ src/NerdCritica.Api/Middleware/ErrorHandlerMiddleware.cs
@@ -39,58 +39,51 @@ public class ErrorHandlerMiddleware
         {
                 
             _logger.LogError(ExceptionDetailsHelper.GetExceptionDetails(ex, context, 400));
-            if (context.Response.HasStarted)
-            {
-                _logger.LogWarning("A resposta já foi iniciada, " +
-       "o manipulador de erros não será executado.");
-                throw;
-            }
+
+            HandleExceptionAfterResponseStarted(context, ex);
+
             await  _exceptionHandler.HandleValidationException(context, ex);
         }
         catch (NotFoundException ex)
         {
             _logger.LogError(ExceptionDetailsHelper.GetExceptionDetails(ex, context, 404));
-            if (context.Response.HasStarted)
-            {
-                _logger.LogWarning("A resposta já foi iniciada, " +
-          "o manipulador de erros não será executado.");
-                throw;
-            }
+
+            HandleExceptionAfterResponseStarted(context, ex);
+
             await _exceptionHandler.HandleNotFoundException(context, ex);
         }
         catch (BadRequestException ex)
         {
 
             _logger.LogError(ExceptionDetailsHelper.GetExceptionDetails(ex, context, 400));
-            if (context.Response.HasStarted)
-            {
-                _logger.LogWarning("A resposta já foi iniciada, " +
-      "o manipulador de erros não será executado.");
-                throw;
-            }
+
+            HandleExceptionAfterResponseStarted(context, ex);
+
             await _exceptionHandler.HandleBadRequestException(context, ex);
         } catch (CreateUserException ex)
         {
             _logger.LogError(ExceptionDetailsHelper.GetExceptionDetails(ex, context, 409));
-            if (context.Response.HasStarted)
-            {
-                _logger.LogWarning("A resposta já foi iniciada, " +
-      "o manipulador de erros não será executado.");
-                throw;
-            }
+
+            HandleExceptionAfterResponseStarted(context, ex);
+
             await _exceptionHandler.HandleCreateUserException(context, ex);
         }
         catch (Exception ex)
         {
             _logger.LogError(ExceptionDetailsHelper.GetExceptionDetails(ex, context, 500));
 
-            if (context.Response.HasStarted)
-            {
-                _logger.LogWarning("A resposta já foi iniciada, " +
-       "o manipulador de erros não será executado.");
-                throw;
-            }
+            HandleExceptionAfterResponseStarted(context, ex);
+
             await _exceptionHandler.HandleGeneralException(context, ex);
+        }
+    }
+
+    private void HandleExceptionAfterResponseStarted(HttpContext context, Exception ex)
+    {
+        if (context.Response.HasStarted)
+        {
+            _logger.LogWarning("A resposta já foi iniciada, o manipulador de erros não será executado.");
+            throw ex;
         }
     }
 }
